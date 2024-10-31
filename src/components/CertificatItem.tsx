3dd1714517c8b6e222dd43e76997749c09e1f87d
Abdel-kader <kabdel552@gmail.com>
2024-10-31 20:51:59 +0000

    finalizing th website

--- src/components/CertificatItem.tsx
+++ src/components/CertificatItem.tsx
@@ -1,37 +1,28 @@
 import React from 'react';
-import { motion } from "framer-motion"
-
 
 export const CertificatItem = ({title, image, pos, desc, program}: {title: string, image: string, pos: string, desc: string, program?: string}) => {
     return (
-            <section className="m-12">
+            <section className="m-12 mb-24">
 
-                <div className="grid grid-cols-2 mb-5">
-                    {pos === 'left' && <div className="flex">
-                        <motion.img initial={{x: 300, opacity: 0}}
-                                    animate={{x: 0, opacity: 1}}
-                                    exit={{x: -300, opacity: 0}}
-                                    src={image}
-                                    transition={{
-                                        ease: "linear",
-                                        duration: 2,
-                                        x: {duration: 1}
-                                    }}
-                                    className="h-[320px] w-[400px]"
-                                    alt="team-building-image"/>
-                    </div>}
-                    <div className="ml-12 flex flex-col">
-                        <h3 className="font-bold text-xl mb-5 underline underline-offset-8 decoration-4 decoration-primary">Présentation</h3>
+                <div className="grid grid-cols-2 gap-4 mb-10">
+                    {pos === 'left' &&
+                        <img
+                            src={image}
+                            className="h-[320px] w-[400px]"
+                            alt="team-building-image"/>
+                    }
+                    <div className="flex flex-col">
+                        <h3 className="font-bold text-xl mb-5 underline underline-offset-8 decoration-4 decoration-primary">{title}</h3>
 
                         <span
-                            className="font-medium text-primary mt-4">
+                            className="font-medium text-primary text-justify mt-4">
                             {desc}
                         </span>
                         <div className="flex justify-evenly">
                             <div
                                 className="flex flex-col border-primary w-40 rounded mt-10 mb-7 border justify-center h-12 align-bottom">
                                 <a
-                                    href={"doc"} rel="noopener noreferrer" target='_blank'
+                                    href={program} rel="noopener noreferrer" target='_blank'
                                     className=" text-md text-primary text-center">Voir le programme
                                 </a>
                             </div>
@@ -45,19 +36,14 @@ export const CertificatItem = ({title, image, pos, desc, program}: {title: strin
                         </div>
                     </div>
 
-                    {pos === 'right' && <div className="flex">
-                        <motion.img initial={{x: 300, opacity: 0}}
-                                    animate={{x: 0, opacity: 1}}
-                                    exit={{x: -300, opacity: 0}}
-                                    src={image}
-                                    transition={{
-                                        ease: "linear",
-                                        duration: 2,
-                                        x: {duration: 1}
-                                    }}
-                                    className="h-[320px] w-[400px]"
-                                    alt="team-building-image"/>
-                    </div>}
+                    {pos === 'right' &&
+                        <div className="flex flex-1 justify-end">
+                            <img
+                                src={image}
+                                className="h-[320px] w-[400px]"
+                                alt="team-building-image"/>
+                        </div>
+                    }
                 </div>
 
             </section>
--- src/pages/CertificatPage.tsx
+++ src/pages/CertificatPage.tsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import certificatBanner from "../assets/banners/certificat.jpg";
 import com from '../assets/certifications/com.png'
+import diplomatie from '../assets/certifications/diplomatie.png'
+import finance from '../assets/certifications/finance.png'
+import marche from '../assets/certifications/marche.png'
 import Banner from "../components/layout/Banner";
 import {CertificatItem} from "../components/CertificatItem";
 
@@ -15,7 +18,31 @@ const CertificatPage = () => {
                 "communication politique est l'\n" +
                 "art de transmettre des messages et des idées à des fins politiques.\n" +
                 "Elle englobe divers moyens de diffusion : les discours, les débats, les médias traditionnels et\n" +
-                "numériques, ainsi que les campagnes publicitaires"} pos="left" program={"com"}  />
+                "numériques, ainsi que les campagnes publicitaires"} pos="left" program={require("../assets/certifications/communication.pdf")}  />
+
+            <CertificatItem title={"Programme de Certification Diplomatie économique et diplomatie d’affaires"} image={diplomatie} desc={"La formation en diplomatie économique et diplomatie d’affaires vise à\n" +
+                "doter les participants des compétences et des connaissances\n" +
+                "nécessaires pour naviguer efficacement dans le monde des affaires\n" +
+                "internationales et des relations économiques entre pays. La Diplomatie\n" +
+                "économique représente la fonction de politique étrangère qui fait la\n" +
+                "liaison entre les démarches de politique étrangère et l’aisance\n" +
+                "économique des citoyens d’un pays et a comme objectif l’utilisation de\n" +
+                "tous les instruments de politique étrangère en vue de la promotion des\n" +
+                "intérêts économiques des entrepreneurs et de l’État."} pos="right" program={require("../assets/certifications/diplomatie.pdf")}  />
+
+
+            <CertificatItem title={"Programme de certification Responsable Administratif et financier"} image={finance}
+                            desc={"Le responsable administratif et financier (RAF) est un poste stratégique au sein d’une entreprise. Le service administratif et financier est le pôle qui se charge de définir et de " +
+                                "conduire l’organisation interne et la budgétisation d’une entreprise. C’est l’un des services les plus importants au sein d’une société."}
+                            pos="left" program={require("../assets/certifications/finance.pdf")}  />
+
+            <CertificatItem title={"Programme de certification Responsable de passation de marchés publics et partenariat publics privés."} image={marche} desc={"Ce programme de certification offre une couverture\n" +
+                "complète de l'\n" +
+                "ensemble des compétences techniques,\n" +
+                "procédurales et commerciales nécessaires à la bonne\n" +
+                "mise en œuvre de chaque étape du processus de\n" +
+                "passation de marchés et de l’établissement des\n" +
+                "partenariats publics privés."} pos="right" program={require("../assets/certifications/marche.pdf")}  />
         </>
     );
 };
